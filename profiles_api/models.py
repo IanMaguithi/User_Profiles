2cc292bb0a1a1e46a462db8019a6ab3f111b2f2e
IanMaguithi <maguithian13427@gmail.com>
2020-10-10 18:10:53 +0300

    user profile model added

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represents a user profile inside our system
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)  # required when substituting the custom user model in django
+    is_staff = models.BooleanField(default=False)
+
+    object = user_profile_manager()  # object manager required
+
+    USERNAME_FIELD = 'email'  # user to use email instead of username to sign in
+    REQUIRED_FIELDS = ['name']
+
+    # helper functions
+    def get_full_name(self):
+        """Used to get a users full name"""
+        return self.name
+
+    def get_short_name(self):
+        """Used to get the users short name"""
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to a string"""
+        return self.email
