7f1c95940137cfaa52a78e910c6c89c5fac1ba78
IanMaguithi <maguithian13427@gmail.com>
2020-10-11 14:47:37 +0300

    created post function in  api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import HalloSerializer
 
 
 # Create your views here.
 class HalloAPIView(APIView):
     """Test APIView"""
+
+    serializer_class = HalloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -17,3 +22,17 @@ class HalloAPIView(APIView):
             'is mapped manually to URLS']
 
         return Response({'message': 'Hallo', 'an_apiview': an_api_view})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        serializer = HalloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = f'Hallo {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)  # invalid data
